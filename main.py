cd95ad71d853efb0f0ca08e9facf753efe6b299d
Hender <hlin01@g.ucla.edu>
2025-02-25 19:00:36 -0600

    initial setup v2

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
 # main.py
+import os
+from dotenv import load_dotenv
+from llama_index.llms.openai import OpenAI
+
+load_dotenv()
+os.environ["OPENAI_API_KEY"]
+
+with open("anonymized_data_cl1.txt", "r") as f:
+    notes = f.readlines()
+
+documents = [
+    Document(text=note.strip() for note in notes)
+]
+
